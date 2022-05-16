120477795950b1c8c476e17697f8f2bdcabf439d
Dmitry Knauer <knauerdm@mail.ru>
2022-05-17 01:41:57 +0800

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Dmitry Knauer on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
